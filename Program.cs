a742339124a4b9fa0375dd30dd89cf694c5332f6
Harit Saxena <137415971+haritsaxenaais@users.noreply.github.com>
2023-10-21 21:27:17 +0530

    Added RAG and embeddings

--- Program.cs
+++ Program.cs
@@ -1,48 +1,76 @@
-﻿using System.Net;
-using System.Text;
-using System.Text.Json;
-using System.Text.RegularExpressions;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Plugins.Memory;
+using Microsoft.SemanticKernel.Text;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 
-// Add Azure OpenAI package
-
-// Build a config object and retrieve user settings.
 IConfiguration config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
+
 string aoaiEndpoint = config["AzureOAIEndpoint"] ?? "";
 string aoaiApiKey = config["AzureOAIKey"] ?? ""; 
 string aoaiModel = config["AzureOAIModelName"] ?? "gpt-3.5-turbo";
-
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine(aoaiModel);
 
 // Initialize the kernel
 IKernel kernel = Kernel.Builder
+    .WithLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
     .WithAzureChatCompletionService(aoaiModel, aoaiEndpoint, aoaiApiKey)
     .Build();
+
+// Download a document and create embeddings for it
+ISemanticTextMemory memory = new MemoryBuilder()
+    .WithLoggerFactory(kernel.LoggerFactory)
+    .WithMemoryStore(new VolatileMemoryStore())
+    .WithAzureTextEmbeddingGenerationService("TextEmbeddingAda002_1", aoaiEndpoint, aoaiApiKey)
+    .Build();
+
+string collectionName = "net7perf";
+using (HttpClient client = new())
+{
+    string s = await client.GetStringAsync("https://devblogs.microsoft.com/dotnet/performance_improvements_in_net_7");
+    List<string> paragraphs =
+        TextChunker.SplitPlainTextParagraphs(
+            TextChunker.SplitPlainTextLines(
+                WebUtility.HtmlDecode(Regex.Replace(s, @"<[^>]+>|&nbsp;", "")),
+                128),
+            1024);
+    for (int i = 0; i < paragraphs.Count; i++)
+        await memory.SaveInformationAsync(collectionName, paragraphs[i], $"paragraph{i}");
+}
 
 // Create a new chat
 IChatCompletion ai = kernel.GetService<IChatCompletion>();
 ChatHistory chat = ai.CreateNewChat("You are an AI assistant that helps people find information.");
-
 StringBuilder builder = new();
-
-using (HttpClient client = new())
-{
-    string s = await client.GetStringAsync("https://devblogs.microsoft.com/dotnet/performance_improvements_in_net_7");
-    s = WebUtility.HtmlDecode(Regex.Replace(s, @"<[^>]+>|&nbsp;", ""));
-    chat.AddUserMessage("Here's some additional information: " + s); // uh oh!
-}
 
 // Q&A loop
 while (true)
 {
     Console.Write("Question: ");
-    chat.AddUserMessage(Console.ReadLine()!);
+    string question = Console.ReadLine()!;
+
+    builder.Clear();
+    await foreach (var result in memory.SearchAsync(collectionName, question, limit: 3))
+        builder.AppendLine(result.Metadata.Text);
+    int contextToRemove = -1;
+    if (builder.Length != 0)
+    {
+        builder.Insert(0, "Here's some additional information: ");
+        contextToRemove = chat.Count;
+        chat.AddUserMessage(builder.ToString());
+    }
+
+    chat.AddUserMessage(question);
 
     builder.Clear();
     await foreach (string message in ai.GenerateMessageStreamAsync(chat))
@@ -53,5 +81,6 @@ while (true)
     Console.WriteLine();
     chat.AddAssistantMessage(builder.ToString());
 
+    if (contextToRemove >= 0) chat.RemoveAt(contextToRemove);
     Console.WriteLine();
 }
